dcf0bbf10afd7d49294359f2d52f0b0a86f9d7ff
David Collini <9niwo957@duck.com>
2024-10-19 10:03:49 -0400

    Sped up and Simplified code
    - Compressed Images and converted to webp
    - Removed needless classes from html
    - Added comments to css to help contributors

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ menuToggle.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
 
-let checkboxes = document.querySelectorAll(".tooltip")
+let checkboxes = document.querySelectorAll(".checker_row div")
 
 for (let i=0; i<checkboxes.length; i++){
     let currenttooltip = checkboxes[i];
-    let tooltiptext = currenttooltip.querySelector(".tooltiptext")
+    let tooltiptext = currenttooltip.querySelector(".checker_row pre")
 
     currenttooltip.addEventListener("mouseenter", function(){
     tooltiptext.style.visibility = "visible";
