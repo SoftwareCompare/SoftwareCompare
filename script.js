1a3c7a6d7051f9290f57a1fbfa91f33aaac55ab0
David Collini <9niwo957@duck.com>
2024-11-05 19:47:00 -0500

    Fix tooltips not needing javascript for mouse devices

--- script.js
+++ script.js
@@ -7,19 +7,6 @@ menuToggle.addEventListener('click', () => {
 
 let checkboxes = document.querySelectorAll(".checker_row div")
 
-for (let i=0; i<checkboxes.length; i++){
-    let currenttooltip = checkboxes[i];
-    let tooltiptext = currenttooltip.querySelector(".checker_row pre");
-
-    currenttooltip.addEventListener("mouseenter", function(){
-    tooltiptext.style.visibility = "visible";
-    
-    })
-    currenttooltip.addEventListener("mouseleave", function(){
-        tooltiptext.style.visibility = "collapse";
-        
-        })
-}
 for (let i = 0; i < checkboxes.length; i++) {
     let currentTooltip = checkboxes[i];
     let tooltipText = currentTooltip.querySelector(".checker_row pre");
