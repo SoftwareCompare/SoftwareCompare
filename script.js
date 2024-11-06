6002f6c36c1d71e289eff5ff2e0dc4c17e6ed288
David Collini <9niwo957@duck.com>
2024-11-05 20:21:52 -0500

    Fix mobile back

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ for (let i = 0; i < checkboxes.length; i++) {
     let currentTooltip = checkboxes[i];
     let tooltipText = currentTooltip.querySelector(".checker_row pre");
   
+    currentTooltip.addEventListener("touchstart", function() {
+      tooltipText.style.visibility = "visible";
+    });
+  
     document.addEventListener("touchstart", function(event) {
       if (!currentTooltip.contains(event.target)) {
         tooltipText.style.visibility = "hidden";
